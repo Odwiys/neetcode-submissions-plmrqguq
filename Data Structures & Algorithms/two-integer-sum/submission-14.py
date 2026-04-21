bb79eaac666639956b2356ec52a5003785956d0f
Mathias Loh <99743435+Odwiys@users.noreply.github.com>
2026-04-21 22:02:30 +0800

    Add: two-integer-sum - submission-14

---
+++ Data Structures & Algorithms/two-integer-sum/submission-14.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        i, j = 0, 0
+        seen = {}
+
+        for x, num in enumerate(nums):
+            compliment = target - num
+            if compliment in seen:
+                i = seen[compliment]
+                j = x
+                return [i, j]
+            else:
+                # add num and index
+                seen[num] = x
+
+        return [i, j]
